e0b3479659d6420c4afe89a2e33c530e5c834635
Mohd Faraz <androiabledroid@gmail.com>
2019-11-13 21:41:17 +0530

    Rolex: fix up

--- omni_rolex.mk
+++ omni_rolex.mk
@@ -14,12 +14,9 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_NAME := omni_rolex
